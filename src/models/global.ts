b01722a51e0e51015076204921af80f993a0f30b
xuzhao <twtyjvkg@foxmail.com>
2022-09-14 16:01:54 +0800

    改造成dva模式

--- src/models/global.ts
+++ src/models/global.ts
@@ -1,21 +1,15 @@
 import { Effect, SubscriptionAPI } from 'dva';
-import { Reducer } from 'redux';
 import { extractAccessTokenFromHash, setAccessToken } from '@/utils/token';
-import { getResponseList } from '@/utils/request';
-import { queryCategoryList } from '@/services/blog/category';
 
 interface GlobalModelState {
-  menuData: Array<any>;
 }
 
 interface GlobalModelType {
   namespace: 'global';
   state: GlobalModelState;
   effects: {
-    initBlog: Effect;
   };
   reducers: {
-    saveMenuData: Reducer<GlobalModelState>;
   };
   subscriptions: {
     setup: (props: SubscriptionAPI) => void;
@@ -25,24 +19,10 @@ interface GlobalModelType {
 const GlobalModel: GlobalModelType = {
   namespace: 'global',
   state: {
-    menuData: [],
   },
   effects: {
-    *initBlog({ callback }, { call, put }) {
-      yield put({
-        type: 'saveMenuData',
-        payload: getResponseList(yield call(queryCategoryList, { level: 1 }))
-      });
-      if (callback) callback();
-    },
   },
   reducers: {
-    saveMenuData(state, action) {
-      return {
-        ...state,
-        menuData: action.payload
-      };
-    },
   },
   subscriptions: {
     setup({ history }) {
